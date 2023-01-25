4fd827a62e002a75dd613c56613ff74070d40671
sarojlimbu0731 <sarojlimbu0731@gmail.com>
2023-01-26 00:01:56 +0545

    setup react-router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes,Route } from "react-router-dom";
+import { SearchPage } from "./pages/SearchPage";
+import { UserDetail } from "./pages/UserDetail";
 
 function App() {
   return (
-    <div className="App">
-      this is app
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<SearchPage/>}/>
+        <Route path="/userdetail" element={<UserDetail/>} />
+      </Routes>
+    </Router>
   );
 }
 
