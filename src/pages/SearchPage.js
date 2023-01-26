16953db75f824987b07de1f074a468142f43171d
sarojlimbu0731 <sarojlimbu0731@gmail.com>
2023-01-26 09:26:59 +0545

    updated search field in search page

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,7 +1,44 @@
-import React from 'react'
+import React, { useState } from "react";
+import './searchPage.css'
+
+import { Button, Input, Space } from "antd";
 
 export const SearchPage = () => {
+    const[searchKey,setSearchkey]=useState("")
+    
+    const handleSubmit=e=>{
+       if(searchKey !==""){
+        e.preventDefault()
+        console.log("button clicked")
+       }
+    }
+ 
+
   return (
-    <div>SearchPage</div>
-  )
-}
+    <div className="searchcontainer">
+    
+
+      <Space direction="horizontal">
+        <Input
+          size="large"
+          status="warning"
+          required
+          onChange={e=>setSearchkey(e.target.value)}
+          placeholder="Github Username"
+          style={{ width: 400, height: 50 }}
+        />
+
+        <Button
+          danger
+          type="primary"
+          onClick={handleSubmit}
+          style={{ width: 200, height: 50, fontSize: 18 }}
+        >
+          Generate
+        </Button>
+      </Space>
+
+    </div>
+  );
+};
+
